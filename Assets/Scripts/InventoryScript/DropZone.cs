8252a79d40dde48ff514f4e19e7a3e743d8d573d
lộc trương <noneednowok@gmail.com>
2024-07-19 13:32:11 +0700

    commit again

--- Assets/Scripts/InventoryScript/DropZone.cs
+++ Assets/Scripts/InventoryScript/DropZone.cs
@@ -1,43 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class DropZone : MonoBehaviour, IDropHandler
+public class DropZone : MonoBehaviour
 {
-    public enum DropZoneType
+    // Start is called before the first frame update
+    void Start()
     {
-        Inventory,
-        Weapon
+        
     }
 
-    public DropZoneType dropZoneType;
-
-    public void OnDrop(PointerEventData eventData)
+    // Update is called once per frame
+    void Update()
     {
-        DraggableItem draggableItem = eventData.pointerDrag.GetComponent<DraggableItem>();
-        if (draggableItem != null)
-        {
-            Transform originalSlot = draggableItem.OriginalParent;
-            Transform newSlot = transform;
-
-            if (newSlot.childCount > 0)
-            {
-                // Nếu slot này đã có item, hoán đổi vị trí
-                Transform existingItem = newSlot.GetChild(0);
-                existingItem.SetParent(originalSlot);
-                existingItem.localPosition = Vector3.zero;
-
-                // Cập nhật slot hiện tại của item cũ
-                DraggableItem existingDraggableItem = existingItem.GetComponent<DraggableItem>();
-                if (existingDraggableItem != null)
-                {
-                    existingDraggableItem.OriginalParent = originalSlot;
-                }
-            }
-
-            // Di chuyển item được kéo vào slot này
-            draggableItem.transform.SetParent(newSlot);
-            draggableItem.transform.localPosition = Vector3.zero;
-            draggableItem.OriginalParent = newSlot; // Cập nhật slot hiện tại của item mới
-        }
+        
     }
 }
